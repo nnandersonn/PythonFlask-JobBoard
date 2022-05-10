6be3d729b20e71af300f5900212ca417787af616
Nick Anderson <i.anderson.nick@gmail.com>
2022-05-10 12:59:08 -0700

    first section complete

---
+++ jobs/app.py
@@ -0,0 +1,11 @@
+from flask import render_template, Flask
+
+app = Flask(__name__)
+
+@app.route('/jobs')
+@app.route('/')
+def jobs():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
